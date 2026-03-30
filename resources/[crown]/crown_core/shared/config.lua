e4c21a0f2619e2542d05f065e0d97db76b019afc
JurassicWaste <info@jurassic-waste.co.uk>
2026-03-30 11:06:46 +0100

    new

---
+++ resources/[crown]/crown_core/shared/config.lua
@@ -0,0 +1,19 @@
+CrownConfig = {}
+
+CrownConfig.Debug = true
+CrownConfig.StartCash = 250.00
+CrownConfig.StartBank = 1250.00
+CrownConfig.Locale = "en-GB"
+
+CrownConfig.DefaultJob = {
+    name = "unemployed",
+    grade = 0
+}
+
+CrownConfig.UK = {
+    vatRate = 0.20,
+    fuelDutyPerLitre = 0.5295,
+    employeeNIMainRate = 0.08,
+    employeeNIAdditionalRate = 0.02,
+    nationalLivingWage = 12.71
+}
